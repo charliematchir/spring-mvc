8dfdd9ffc1b3df64823a0001bc05288ce06562af
charliematchir <charliematchir@gmail.com>
2023-11-23 19:04:23 +0900

    mvc-first

---
+++ mvc-clone/src/main/java/com/prac/mvcclone/MvcCloneApplication.java
@@ -0,0 +1,13 @@
+package com.prac.mvcclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcCloneApplication.class, args);
+    }
+
+}
---
+++ mvc-clone/src/test/java/com/prac/mvcclone/MvcCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.mvcclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
